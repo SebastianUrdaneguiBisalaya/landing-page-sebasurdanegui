e2b4640c8c9467ca2ac6387029240f6d8cc416c9
Sebastian Urdanegui Bisalaya <sebasurdanegui@gmail.com>
2024-12-26 16:53:10 -0500

    feat: implement the background pattern on layout for all landing page.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FD5F00",
+        "gray-custom": "#606060",
+        "txt-custom-gray": "#e1dbdb",
+      },
+      fontFamily: {
+        "bricolage-grotesque": ["Bricolage Grotesque", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
