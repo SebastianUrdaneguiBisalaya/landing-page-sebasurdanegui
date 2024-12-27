98bb0ef8f48af73ab2c00f16eb0ecc2490b84188
Sebastian Urdanegui Bisalaya <sebasurdanegui@gmail.com>
2024-12-27 17:26:29 -0500

    feat: implement the contaction section. That is done. The functionality is not ready yet.

---
+++ src/constants/contactItems.ts
@@ -0,0 +1,14 @@
+export const contactItems = [
+  {
+    logo: "linkedIn",
+    alt: "LinkedIn Profile Logo",
+    path: "/in/sebastianurdaneguibisalaya/",
+    description: "Comunícate conmigo mediante un mensaje de LinkedIn.",
+  },
+  {
+    logo: "gmail",
+    alt: "Gmail Profile Logo",
+    path: "sebasurdanegui@gmail.com",
+    description: "Comunícate conmigo mediante un correo de Gmail.",
+  },
+];
