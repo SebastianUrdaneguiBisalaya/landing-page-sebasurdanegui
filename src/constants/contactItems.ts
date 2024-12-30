b4fa96111f1b8f0a77158174a1fb956a508e8f34
Sebastian Urdanegui Bisalaya <sebasurdanegui@gmail.com>
2024-12-29 23:14:57 -0500

    fix: improve the dynamic component of card project. add the link to show info about what is the way for user to contact me.

--- src/constants/contactItems.ts
+++ src/constants/contactItems.ts
@@ -4,11 +4,14 @@ export const contactItems = [
     alt: "LinkedIn Profile Logo",
     path: "/in/sebastianurdaneguibisalaya/",
     description: "Comunícate conmigo mediante un mensaje de LinkedIn.",
+    pathToGo: "https://www.linkedin.com/in/sebastianurdaneguibisalaya/",
   },
   {
     logo: "gmail",
     alt: "Gmail Profile Logo",
     path: "sebasurdanegui@gmail.com",
     description: "Comunícate conmigo mediante un correo de Gmail.",
+    pathToGo:
+      "mailto:sebasurdanegui@gmail.com?subject=Consulta%20&body=Hola,%20quisiera%20saber%20más%20sobre%20sus%20servicios.",
   },
 ];
